8d650fe2bea75fbb22343f7b65c1bb4d586bd7fd
Alicia <alicia.arenzana@gmail.com>
2019-11-28 17:08:24 +0100

    added vega to the proyect; also redistributed the files

---
+++ data/data.js
@@ -0,0 +1,362 @@
+let data = [
+        {
+          "year": 2020,
+          "source": "someLink",
+          "default": false,
+          "title": "gsdfg",
+          "cat": "a",
+          "size": 1,
+          "value": 0.657,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2030,
+          "source": "someLink",
+          "default": true,
+          "title": "aserwsdfs",
+          "cat": "a",
+          "size": 1,
+          "value": 0.822,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2040,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfe",
+          "cat": "eadsfjkewroiewhoiweroiqweradsfkhwerqe qewr dsfkjasdfew",
+          "size": 2,
+          "value": 0.98,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2020,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfe",
+          "cat": "d",
+          "size": 2,
+          "value": 0.657,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2022,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfe",
+          "cat": "b",
+          "size": 3,
+          "value": 0.691,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2035,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfeawa",
+          "cat": "b",
+          "size": 3,
+          "value": 0.879,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2040,
+          "source": "someLink",
+          "default": true,
+          "title": "asfea",
+          "cat": "c",
+          "size": 3,
+          "value": 0.98,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2028,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfe",
+          "cat": "c",
+          "size": 1,
+          "value": 0.759,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2032,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "c",
+          "size": 2,
+          "value": 0.853,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2020,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "d",
+          "size": 1,
+          "value": 0.657,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2032,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "d",
+          "size": 3,
+          "value": 0.853,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2040,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "a",
+          "size": 1,
+          "value": 0.98,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2036,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "d",
+          "size": 2,
+          "value": 0.878,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2024,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "c",
+          "size": 3,
+          "value": 0.737,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2035,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "d",
+          "size": 1,
+          "value": 0.879,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2024,
+          "source": "someLink",
+          "default": true,
+          "title": "co2-concentration",
+          "cat": "d",
+          "size": 2,
+          "value": 0.737,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2026,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "a",
+          "size": 3,
+          "value": 0.717,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2030,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "a",
+          "size": 1,
+          "value": 0.822,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2020,
+          "source": "someLink",
+          "default": false,
+          "title": "gsdfg",
+          "cat": "a",
+          "size": 1,
+          "value": 0.657,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2030,
+          "source": "someLink",
+          "default": true,
+          "title": "aserwsdfs",
+          "cat": "a",
+          "size": 1,
+          "value": 0.822,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2040,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfe",
+          "cat": "a",
+          "size": 2,
+          "value": 0.98,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2020,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfe",
+          "cat": "b",
+          "size": 2,
+          "value": 0.657,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2022,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfe",
+          "cat": "d",
+          "size": 3,
+          "value": 0.691,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2035,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfeawa",
+          "cat": "b",
+          "size": 3,
+          "value": 0.879,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2040,
+          "source": "someLink",
+          "default": true,
+          "title": "asfea",
+          "cat": "c",
+          "size": 3,
+          "value": 0.98,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2028,
+          "source": "someLink",
+          "default": false,
+          "title": "asdfe",
+          "cat": "c",
+          "size": 1,
+          "value": 0.759,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2032,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "c",
+          "size": 2,
+          "value": 0.853,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2020,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "f",
+          "size": 1,
+          "value": 0.657,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2032,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "d",
+          "size": 3,
+          "value": 0.853,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2040,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "a",
+          "size": 1,
+          "value": 0.98,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2036,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "e",
+          "size": 2,
+          "value": 0.878,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2024,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "c",
+          "size": 3,
+          "value": 0.737,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2035,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "e",
+          "size": 1,
+          "value": 0.879,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2024,
+          "source": "someLink",
+          "default": true,
+          "title": "co2-concentration",
+          "cat": "d",
+          "size": 2,
+          "value": 0.737,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2026,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "a",
+          "size": 3,
+          "value": 0.717,
+          "scenario": "act-Now"
+        },
+        {
+          "year": 2030,
+          "source": "someLink",
+          "default": false,
+          "title": "co2-concentration",
+          "cat": "f",
+          "size": 1,
+          "value": 0.822,
+          "scenario": "act-Now"
+        }
+      ]
