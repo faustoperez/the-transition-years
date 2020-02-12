c82f4ab9280b862a6c8965721ec5df56bfb82ed7
Alicia <alicia.arenzana@gmail.com>
2020-02-12 12:29:33 +0100

    added the data, connect it with t he vizz and generate the lists

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-let spec = "js/visualization.vg.json"
-var opt = {
+const VEGA_SPEC = 'js/visualization.vg.json';
+const DEFAULT_OPTIONS = {
   defaultStyle: false,
   renderer:"svg",
   width: 600,
@@ -7,49 +7,36 @@ var opt = {
   actions: false,
   config: "js/config.json",
   tooltip: {theme: 'dark'}
+};
+
+function renderList(list) {
+  var $container = document.getElementById('mylist');
+  var items = list.map(function(arr) {
+    return '<li><a href="' + arr.source + '" title="' + arr.title + '">' + (arr['Source Name'] || arr.title) + '</a></li>';
+  });
+  var tpl = '<ol>' + items.join('') + '</ol>';
+  console.log(tpl);
+  $container.innerHTML = tpl;
 }
-const newData = data.map(function(d) {
+
+function initalize() {
+  // dom ready
+  const newData = data.map(function(d) {
     return Object.assign(d, { title: d.title.match(/(([\w]+(?:[\W\n|\W\.|\W\,]+[\w\n|\w\.|\w\,]+){0,2})+)/g) });
-});
-vegaEmbed('#view',spec, opt).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  result.view.insert('fiction',newData).runAsync();
-  //result.view.addSignalListener('tooltip', function(name, value) {
-  //      console.log('WIDTH: ' + value);
-  //      })
-  }).catch(console.error);
+  });
 
-//
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
+  vegaEmbed('#view',VEGA_SPEC, DEFAULT_OPTIONS)
+    .then(function(result) {
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+      result.view.insert('fiction',newData).runAsync();
+      result.view.insert('backNormal',newData).runAsync();
+      //result.view.addSignalListener('tooltip', function(name, value) {
+      //      console.log('WIDTH: ' + value);
+      //      })
+    })
+    .catch(console.error);
+  
+  renderList(data);
+}
 
-
-
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//     // Get the header
-//     var header = document.getElementById("myHeader");
-//     var sizeD = document.getElementById("view");
-
-//     var about = sizeD.offsetHeight + sizeD.offsetTop - header.offsetHeight - 50
-//     console.log(about)
-//     // Get the offset position of the navbar
-//     var sticky = header.offsetTop;
-
-//   if (window.pageYOffset > sticky && window.pageYOffset < about && window.innerWidth > 1024) {
-//     //header.classList.remove("fixEl");
-//     header.classList.add("sticky");
-//   } 
-//   else if(window.pageYOffset > about && window.innerWidth > 1024) {
-//     header.classList.remove("sticky");
-//     //header.classList.add("fixEl");
-//     header.style.position = "absolute";
-//     header.style.top = about + 'px';
-//   }
-//   else {
-//     header.classList.remove("sticky");
-//     header.style.removeProperty('position')
-//     header.style.removeProperty('top')
-//   }
-// }   
-
+document.addEventListener('DOMContentLoaded', initalize);
