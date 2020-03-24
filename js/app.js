37db5d99989b0a8902ff625ade3ec2210b4ed255
Alvaro Leal <pursuance@gmail.com>
2020-03-24 16:51:47 +0100

    Add click events and fix typo

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function renderList(list) {
   var $container = document.getElementById('mylist');
   var items = list.map(function(arr) {
     return '<li><a href="' + arr.source + '" title="' + arr.title + '" id='+arr.id +' target="_blank">'
-      + (arr['Source Name'] || arr.title) 
+      + (arr['Source Name'] || arr.title)
       + '</a></li>';
   });
   var tpl = '<ol>' + items.join('') + '</ol>';
@@ -13,18 +13,18 @@ function renderList(list) {
 }
 
 function renderLegend(signalObject) {
-  
+
   var $tooltip = document.getElementById('chartTooltip');
-  
-  var tpl = '<h1>' + signalObject.datum.year + ' / '+ signalObject.datum.value 
-          +' ºC (<a href=#'+ signalObject.datum.id +'>'+ signalObject.datum.id +'</a>)</h1>' 
+
+  var tpl = '<h1>' + signalObject.datum.year + ' / '+ signalObject.datum.value
+          +' ºC (<a href=#'+ signalObject.datum.id +'>'+ signalObject.datum.id +'</a>)</h1>'
           + '<p><br>' + signalObject.datum.title.join(' ') + '</br></p>'
 
   $tooltip.innerHTML = tpl;
 
 }
 
-function initalize() {
+function initialize() {
   // dom ready
 
   const $view = document.getElementById('view');
@@ -34,7 +34,7 @@ function initalize() {
   ? $view.offsetWidth
   : $mainLegend.offsetWidth;
 
-  
+
   const DEFAULT_OPTIONS = {
     defaultStyle: false,
     renderer:"svg",
@@ -44,16 +44,16 @@ function initalize() {
     config: "js/config.json"
   };
 
-  
+
   data.map(function(d) {
     return Object.assign(d, { id: (data.indexOf(d) + 1)});
   });
   const newData = data.map(function(d) {
     // return  {...d, title: d.title.match(/(([\w]+(?:[\W\n|\W\.|\W\,]+[\w\n|\w\.|\w\,]+){0,3})+)/g)};
     return  {...d, title: d.title.match(/((\w+\W+){0,4})/g)};
-    
+
   });
-   
+
   vegaEmbed('#view',VEGA_SPEC, DEFAULT_OPTIONS)
     .then(function(result) {
       // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -69,8 +69,8 @@ function initalize() {
       });
     })
     .catch(console.error);
-  
+
   renderList(data);
 }
 
-document.addEventListener('DOMContentLoaded', initalize);
+document.addEventListener('DOMContentLoaded', initialize);
